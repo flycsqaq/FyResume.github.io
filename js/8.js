746a2be82c49d699b5bbadc5cbeed559c61116bd
zhoufeiyu <119775549@qq.com>
2019-07-08 21:53:51 +0800

    code clean

--- js/8.js
+++ js/8.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{637:function(n,e,t){"use strict";t.r(e);var o=t(1),u=t.n(o);e.default=function(){return u.a.createElement("div",null,"home")}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{639:function(n,e,t){"use strict";t.r(e);var o=t(1),u=t.n(o);e.default=function(){return u.a.createElement("div",null,"home")}}}]);
